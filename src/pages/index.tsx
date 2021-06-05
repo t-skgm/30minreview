988e0c048f929caa5682996af132b3482233132e
t-skgm <skgm0301@gmail.com>
2021-06-05 22:00:08 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Layout } from '../components/Layout'
-import { PostPreview } from '../components/Post'
-
 import { getAllPosts, PostContentType } from '../lib/api'
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import { blogConfig, globalNav } from '../lib/constants'
+
+import { Layout } from '../components/Layout'
+import { PostPreview } from '../components/Post'
+import { Container } from '../components/Container'
 
 type Props = {
   allPosts: PostContentType[]
@@ -31,7 +32,7 @@ const Index: React.VFC<Props> = ({ allPosts }) => {
         </ul>
       </nav>
 
-      <div>
+      <div className="mb-32 max-w-4xl mx-auto">
         {allPosts.length > 0 && (
           <div>
             {allPosts.map(post => (
@@ -40,6 +41,8 @@ const Index: React.VFC<Props> = ({ allPosts }) => {
           </div>
         )}
       </div>
+
+      {/* Paginate. */}
     </Layout>
   )
 }
